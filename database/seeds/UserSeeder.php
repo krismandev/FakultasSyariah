23cd0cab90cdb1edef6f95ff0ec4774c3a2fcf4c
krismandev <krismanpratama@gmail.com>
2021-06-19 09:14:21 +0700

    seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'nama' => 'Krisman Pratama',
+            'email' => 'krismanpratama@gmail.com',,
+            'role' => 's',
+            'password' => bcrypt('admin123456'),
+        ]);
+
+        User::create([
+            'nama' => 'Admin',
+            'email' => 'syariah@uinjambi.ac.id',
+            'role' => 's',
+            'password' => bcrypt('admin123456')
+        ]);
+
+    }
+}
